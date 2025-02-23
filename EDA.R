c76326fc99fe5b452a0e2754d3b88321d42b7510
b8niiii <120273016+b8niiii@users.noreply.github.com>
2025-02-23 18:41:18 +0100

    Seasonal analysis with STL decomposition and plot

--- EDA.R
+++ EDA.R
@@ -181,3 +181,153 @@ pairs(maurienne[, sapply(maurienne, is.numeric)])
 
 #m <- ggplot(maurienne, aes(x = date, y = mean_temp)) + geom_line() + labs(title = "Maurienne Valley Temperature Over 20 Years", x = "Date", y = "Mean Temperature (°C)") + theme_minimal()
 #ggplotly(m)
+
+#SEASONAL ANALYSIS:
+# Create a new column for the month of the year
+maurienne <- maurienne %>% mutate(month = month(date, label = TRUE))
+susa <- susa %>% mutate(month = month(date, label = TRUE))
+# Create a new column for the year
+maurienne <- maurienne %>% mutate(year = year(date))
+susa <- susa %>% mutate(year = year(date))
+# Now we can use facetting to see how the variable behaves across each season
+
+
+
+# Calculate the mean temperature per year for each month
+maurienne_yearly <- maurienne %>% # Calculate the mean temperature per year for each month
+  group_by(year, month) %>% # Group by year and month
+  summarize(mean_temp_year = mean(mean_temp, na.rm = TRUE)) # Calculate the mean temperature
+
+susa_yearly <- susa %>%
+  group_by(year, month) %>%
+  summarize(mean_temp_year = mean(mean_temp, na.rm = TRUE))
+
+
+# Create seasonal plots for Maurienne Valley
+ggplot(maurienne, aes(x = year, y = mean_temp)) + # Plot the original data
+  geom_line(color = "tomato", alpha = 0.5) + # Add a line plot
+  geom_line(data = maurienne_yearly, aes(x = year, y = mean_temp_year), # Add a line plot for yearly means
+            color = "blue", size = 0.5) +
+  facet_wrap(~ month, ncol = 4) + # Facet by month
+  labs(
+    title = "Maurienne Valley Temperature by Month across Years",
+    x = "Year",
+    y = "Mean Temp (°C)"
+  ) +
+  theme_minimal() # Apply a minimal theme
+ggplot(susa, aes(x = year, y = mean_temp)) + # Plot the original data
+  geom_line(color = "tomato", alpha = 0.5) + # Add a line plot
+  geom_line(data = susa_yearly, aes(x = year, y = mean_temp_year), # Add a line plot for yearly means
+            color = "blue", size = 0.5) +
+  facet_wrap(~ month, ncol = 4) + # Facet by month
+  labs(
+    title = "Susa Valley Temperature by Month across Years",
+    x = "Year",
+    y = "Mean Temp (°C)"
+  ) +
+  theme_minimal()
+
+# DO THE SAME PLOTS BUT LOOKING AT PRECIPITATIONS
+
+maurienne_yearly_prec <- maurienne %>%
+  group_by(year, month) %>%
+  summarize(mean_prec_year = mean(total_precip, na.rm = TRUE))
+
+susa_yearly_prec <- susa %>%
+  group_by(year, month) %>%
+  summarize(mean_prec_year = mean(total_precip, na.rm = TRUE))
+
+# Create seasonal plots of precipitations for Maurienne Valley
+ggplot(maurienne, aes(x = year, y = total_precip)) + # Plot the original data
+  geom_line(color = "tomato", alpha = 0.5) + # Add a line plot
+  geom_line(data = maurienne_yearly_prec, aes(x = year, y = mean_prec_year), # Add a line plot for yearly means
+            color = "blue", size = 0.5) +
+  facet_wrap(~ month, ncol = 4) + # Facet by month
+  labs(
+    title = "Maurienne Valley Precipitations by Month across Years",
+    x = "Year",
+    y = "Mean Prec (mm)"
+  ) +
+  theme_minimal() # Apply a minimal theme
+ggplot(susa, aes(x = year, y = total_precip)) + # Plot the original data
+  geom_line(color = "tomato", alpha = 0.5) + # Add a line plot
+  geom_line(data = susa_yearly_prec, aes(x = year, y = mean_prec_year), # Add a line plot for yearly means
+            color = "blue", size = 0.5) +
+  facet_wrap(~ month, ncol = 4) + # Facet by month
+  labs(
+    title = "Susa Valley Precipitations by Month across Years",
+    x = "Year",
+    y = "Mean Prec (mm)"
+  ) +
+  theme_minimal()
+
+# Same plots but just showing the mean:
+ggplot(maurienne_yearly_prec, aes(x = year, y = mean_prec_year)) +
+  geom_line(color = "blue", size = 0.5) +
+  facet_wrap(~ month, ncol = 4) +
+  labs(
+    title = "Maurienne Valley Precipitations by Month across Years",
+    x = "Year",
+    y = "Mean Prec (mm)"
+  ) +
+  theme_minimal()
+
+ggplot(susa_yearly_prec, aes(x = year, y = mean_prec_year)) +
+  geom_line(color = "blue", size = 0.5) +
+  facet_wrap(~ month, ncol = 4) +
+  labs(
+    title = "Susa Valley Precipitations by Month across Years",
+    x = "Year",
+    y = "Mean Prec (mm)"
+  ) +
+  theme_minimal()
+
+# overlapping them:
+ggplot() +
+  geom_line(data = maurienne_yearly_prec, 
+            aes(x = year, y = mean_prec_year, color = "Maurienne"), 
+            size = 0.5) +
+  geom_line(data = susa_yearly_prec, 
+            aes(x = year, y = mean_prec_year, color = "Susa"), 
+            size = 0.5) +
+  facet_wrap(~ month, ncol = 4) +
+  labs(
+    title = "Precipitations by Month across Years: Maurienne vs Susa",
+    x = "Year",
+    y = "Mean Precipitation (mm)",
+    color = "Valley"
+  ) +
+  scale_color_manual(values = c("Maurienne" = "blue", "Susa" = "red")) +
+  theme_minimal()
+
+a <- lm(min_temp ~ year, data = maurienne)
+summary(a)
+b <- lm(max_temp ~ year, data = maurienne)
+summary(b)
+c <- lm(mean_temp ~ year, data = maurienne)
+summary(c)
+
+d <- lm(min_temp ~ year, data = susa)
+summary(d)
+e <- lm(max_temp ~ year, data = susa)
+summary(e)
+f <- lm(mean_temp ~ year, data = susa)
+summary(f)
+
+
+# LET'S TRY BY USING STL DECOMPOSITION
+# Again assuming monthly data with frequency = 12
+# Example: creating a time series object from daily data
+# temp_data: your vector of daily temperature observations
+temp_ts_susa <- ts(susa$mean_temp, start = c(1980, 1), frequency = 365)
+# Decompose the time series using STL
+# Apply STL decomposition on the time series
+stl_decomp_susa <- stl(temp_ts, s.window = "periodic", robust = TRUE)
+
+# Plot the decomposition components
+plot(stl_decomp)
+
+
+temp_ts_maurienne <- ts(maurienne$mean_temp, start = c(1980, 1), frequency = 365)
+stl_decomp_maurienne <- stl(temp_ts_maurienne, s.window = "periodic", robust = TRUE)
+plot(stl_decomp_maurienne)
